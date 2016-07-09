ff6f14bb24eb65d4bbdc0901bd53b0c9cf46af2b
japanoise <japanoise@tfwno.gf>
2016-07-09 23:39:18 +0100

    Fixed spelling mistakes/small nags

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ echo "Making ~/.config/..."
 mkdir --parents --verbose ~/.config
 echo "Copying jekyll-upload.conf..."
 cp --verbose --interactive jekyll-upload.conf ~/.config/
-printf "%s\n%s\n" "jekyll-install will be installed momentarily!" "I just need to ask you a few questions about the install."
-echo "Would you like me to use a symlink? This means if you update the script here by using git pull, there will be no need to reinstall"
+printf "%s\n" "jekyll-upload will be installed momentarily!" "I just need to ask you a few questions about the install."\
+ "Would you like me to use a symlink? This means if you update the script here by using git pull, there will be no need to reinstall"
 asking=true
 while $asking; do
 	read -p "Use symlink? [y/n] > " yn
@@ -34,8 +34,22 @@ else
 fi
 comm=""
 if $symlink; then
-	comm="ln -s \"$(pwd)/jekyll-upload\" \"$install\""
+	comm="ln -s \"$(pwd)/jekyll-upload\" $install"
 else
-	comm="cp \"$(pwd)/jekyll-upload\" \"$install\""
+	comm="cp \"$(pwd)/jekyll-upload\" $install"
 fi
 printf "Here's the command to run:\n%s\n(You may need to use sudo, if you lack the permissions to make files in %s)\n" "$comm" "$install"
+asking=true
+while $asking; do
+	read -p "Run the command now?? [y/n] > " yn
+	case $yn in
+		y)
+			run=true
+			asking=false
+			;;
+		n)
+			run=false
+			asking=false
+			;;
+	esac
+done
